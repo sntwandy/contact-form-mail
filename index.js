2cecaa48d5b45311f10ec4aeee0f0c9d41fe40c8
Wandy Santana <sntwandy@gmail.com>
2020-11-29 22:43:40 -0400

    fix

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(require("./routes/index"));
+app.use(require("./src/routes/index"));
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "src/public")));
 
 app.listen(3000, () => {
   console.log("Server is running on port: 3000");
